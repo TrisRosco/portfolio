637b1694541a731ecf64b8ac019155fc09b3e0af
trisrosco <trisrosco@gmail.com>
2023-05-25 09:29:47 +0100

    React installed and working. File scructure started

---
+++ pf/src/components/app/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is my sick app</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
