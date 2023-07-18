f0076e3a41269ed50392afc34091a5692a15d65a
trisrosco <trisrosco@gmail.com>
2023-07-18 13:53:37 +0100

    nav in position, test background added

--- pf/src/components/app/App.js
+++ pf/src/components/app/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar />
+        <NavBar className="nav-bar" />
         <p>Hey</p>
       </header>
     </div>
--- pf/src/components/navBar/index.js
+++ pf/src/components/navBar/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./styles.css";
+
 // add accordian menu
 const NavBar = () => {
+
+
+
   return (
     <nav className="nav-bar">
       <ul className="nav-bar-list">
         <li className="nav-bar-list-item">
-          <a href="#">About me</a>
+          <a href="about">About me</a>
         </li>
         <li className="nav-bar-list-item">
           <a href="#">Projects</a>
         </li>
         <li className="nav-bar-list-item">
-          <a href="#">Contact</a>
+          <a href="contact">Contact</a>
         </li>
       </ul>
     </nav>
