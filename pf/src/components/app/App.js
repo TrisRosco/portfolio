4529c11190d83715e01760140051e8c0798c70cd
trisrosco <trisrosco@gmail.com>
2023-07-18 13:10:52 +0100

    cleaning up app, preparing for revamp

--- pf/src/components/app/App.js
+++ pf/src/components/app/App.js
@@ -1,33 +1,10 @@
 import "./App.css";
-import ToggleSwitch from "../toggleSwitch";
-import CrtOuter from "../crtOuter";
 import { useEffect, useState } from "react";
-import jeff from "../../assets/images/jeff.jpg";
-import welcome from "../../assets/sounds/welcome.mp3";
 function App() {
-  const [isNVGOn, setIsNVGOn] = useState(false);
 
   useEffect(() => {
     console.log("App loaded");
   }, []);
-
-  useEffect(() => {
-    if (isNVGOn === true) {
-      console.log("nvg mode on");
-    } else {
-      console.log("nvg mode off");
-    }
-  }, [isNVGOn]);
-
-  const nvgMode = () => {
-    setIsNVGOn(!isNVGOn);
-  };
-
-  const playWelcome = () => {
-    const audio = new Audio(welcome);
-    audio.play();
-  };
-
 
   return (
     <div className="App">
