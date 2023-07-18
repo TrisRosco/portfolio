f3f99b4cb1e3c6cf979c6b66641fe4f5f2957c36
trisrosco <trisrosco@gmail.com>
2023-07-18 14:14:17 +0100

    Styling fixes

--- pf/src/components/navBar/index.js
+++ pf/src/components/navBar/index.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+
 
 // add accordian menu
 const NavBar = () => {
+const [dropDownOpen, setDropDownOpen] = useState(false);
 
+const toggleDropDown = () => {
+    setDropDownOpen(!dropDownOpen);
+};
 
 
   return (
     <nav className="nav-bar">
       <ul className="nav-bar-list">
-        <li className="nav-bar-list-item">
-          <a href="about">About me</a>
+        <li className="nav-bar-list-button">
+            <p>About</p>
         </li>
-        <li className="nav-bar-list-item">
-          <a href="#">Projects</a>
+        <li className="nav-bar-list-button">
+            <p onClick={toggleDropDown}>Projects</p>
         </li>
-        <li className="nav-bar-list-item">
-          <a href="contact">Contact</a>
+        <li className="nav-bar-list-button">
+          <p>Contact</p>
         </li>
       </ul>
     </nav>
@@ -24,3 +29,4 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
