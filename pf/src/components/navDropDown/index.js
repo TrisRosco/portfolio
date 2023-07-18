cec598155593b83320999d74678605e5e5098336
trisrosco <trisrosco@gmail.com>
2023-07-18 15:52:11 +0100

    Styled the dropdown menu, added springy

--- pf/src/components/navDropDown/index.js
+++ pf/src/components/navDropDown/index.js
@@ -3,39 +3,42 @@ import "./styles.css";
 import { useAnimate, stagger, motion } from "framer-motion";
 
 const NavDropDown = (props) => {
-
-    return (
-        <div className="nav-drop-down">
-        {props.dropDownOpen && (
-    <motion.div
-      initial="hidden"
-      animate="visible"
-      exit="hidden"
-      variants={{
-        hidden: { opacity: 0, y: -10 },
-        visible: { opacity: 1, y: 0 },
-      }}
-      className="drop-down"
-    >
-      <motion.div className="drop-down-item">
-        <p>Project 1</p>
-      </motion.div>
-      <motion.div className="drop-down-item">
-        <p>Project 2</p>
-      </motion.div>
-      <motion.div className="drop-down-item">
-        <p>Project 3</p>
-      </motion.div>
-      <motion.div className="drop-down-item">
-        <p>Project 4</p>
-      </motion.div>
-      <motion.div className="drop-down-item">
-        <p>Project 5</p>
-      </motion.div>
-    </motion.div>
-  )}
-        </div>
-    );
+  return (
+    <div className="nav-drop-down">
+      {props.dropDownOpen && (
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={{
+            hidden: { opacity: 0, y: -100 },
+            visible: {
+              opacity: 1,
+              y: 0,
+              transition: { delay: 0.2, type: "spring", stiffness: 900 },
+            },
+          }}
+          className="drop-down"
+        >
+          <motion.div className="drop-down-item">
+            <p>HonK</p>
+          </motion.div>
+          <motion.div className="drop-down-item">
+            <p>LITTR</p>
+          </motion.div>
+          <motion.div className="drop-down-item">
+            <p>Something else</p>
+          </motion.div>
+          <motion.div className="drop-down-item">
+            <p>Another thing</p>
+          </motion.div>
+          <motion.div className="drop-down-item">
+            <p>Jeff</p>
+          </motion.div>
+        </motion.div>
+      )}
+    </div>
+  );
 };
 
 export default NavDropDown;
