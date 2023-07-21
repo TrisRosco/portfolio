ac3f2b6eb2408d8a9c12e3f98079610c3a661902
trisrosco <trisrosco@gmail.com>
2023-07-21 11:21:52 +0100

    Routing added, migrated about page

--- pf/src/components/app/App.js
+++ pf/src/components/app/App.js
@@ -1,26 +1,20 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import NavBar from "../navBar";
-import NavDropDown from "../navDropDown";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import About from "../../pages/About";
+import CRT from "../../pages/CRT";
 
 function App() {
-  const [dropDownState, setDropDownState] = useState(false);
 
-  const toggleDropDown = () => {
-    setDropDownState(!dropDownState);
-  };
-
-  useEffect(() => {
-    console.log("App loaded");
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar className="nav-bar" projectClick={toggleDropDown} zIndex="2" />
-        <NavDropDown className="nav-drop-down" dropDownOpen={dropDownState} zIndex="1" />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/crt" element={<CRT />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ pf/src/pages/About/index.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import NavBar from "../../components/navBar";
+import NavDropDown from "../../components/navDropDown";
+import "./styles.css";
+
+
+function About() {
+    const [dropDownState, setDropDownState] = useState(false);
+  
+    const toggleDropDown = () => {
+      setDropDownState(!dropDownState);
+    };
+  
+    useEffect(() => {
+      console.log("App loaded");
+    }, []);
+  
+    return (
+      <div className="about">
+        <header className="about-header">
+          <NavBar className="nav-bar" projectClick={toggleDropDown} zIndex="2" />
+          <NavDropDown className="nav-drop-down" dropDownOpen={dropDownState} zIndex="1" />
+        </header>
+      </div>
+    );
+  }
+  
+  export default About;
+  
