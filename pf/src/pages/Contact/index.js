5b842c7403dbab2efaa5adf46260d8fdc6b3a824
trisrosco <trisrosco@gmail.com>
2023-08-15 10:25:48 +0100

    integrated emailjs

--- pf/src/pages/Contact/index.js
+++ pf/src/pages/Contact/index.js
@@ -1,12 +1,36 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useRef } from "react";
 import "./styles.css";
 import NavBar from "../../components/navBar";
 import PhysicsTest from "../../components/PhysicsTest";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+
   useEffect(() => {
     console.log("Contact loaded");
   }, []);
+
+  const form = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Button pressed");
+    emailjs
+      .sendForm(
+        "service_ovomt55",
+        "template_k4ul5w8",
+        event.target,
+        "ajBbLf733lblmWZ4K"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <div className="contact-container">
@@ -15,13 +39,17 @@ const Contact = () => {
       </header>
       <div className="contact-content">
         <div className="contact-content-item">
-          <p>Suprise, this isn't finished yet!</p>
+          <p> Contact Me </p>
+          <form className="contact-input" ref={form} onSubmit={handleSubmit}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea id="message" name="message" />
+            <input id="submit" type="submit" value="Send" />
+          </form>
         </div>
-        <div className="contact-content-item">
-          <p>But to keep you busy, here's a physics test thingy I made!</p>
-          <PhysicsTest />
-        </div>
-        <div className="contact-content-item"></div>
       </div>
     </div>
   );
