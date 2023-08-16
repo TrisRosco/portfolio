6e1178f32deed60765b0f8d00a2731c4d267bdce
trisrosco <trisrosco@gmail.com>
2023-08-16 13:57:46 +0100

    Added error colours to limits

--- pf/src/pages/Contact/index.js
+++ pf/src/pages/Contact/index.js
@@ -1,11 +1,9 @@
 import { React, useEffect, useRef } from "react";
 import "./styles.css";
 import NavBar from "../../components/navBar";
-import PhysicsTest from "../../components/PhysicsTest";
 import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-
   useEffect(() => {
     console.log("Contact loaded");
   }, []);
@@ -29,7 +27,7 @@ const Contact = () => {
         (error) => {
           console.log(error.text);
         }
-      ); 
+      );
     form.current.reset();
     alert("Message sent!");
   };
@@ -38,18 +36,36 @@ const Contact = () => {
     let input = document.getElementsByName("user_name")[0];
     let limit = document.getElementsByClassName("name-limit")[0];
     limit.innerHTML = input.value.length + "/55";
+
+    if (input.value.length > 54) {
+      limit.classList.add("name-limit-error");
+    } else {
+      limit.classList.remove("name-limit-error");
+    }
   }
 
   function handleEmailCheck() {
     let input = document.getElementsByName("user_email")[0];
     let limit = document.getElementsByClassName("email-limit")[0];
     limit.innerHTML = input.value.length + "/55";
+
+    if (input.value.length > 54) {
+      limit.classList.add("email-limit-error");
+    } else {
+      limit.classList.remove("email-limit-error");
+    }
   }
 
   function handleMessageCheck() {
     let input = document.getElementById("message");
     let limit = document.getElementsByClassName("message-limit")[0];
     limit.innerHTML = input.value.length + "/255";
+
+    if (input.value.length > 254) {
+      limit.classList.add("message-limit-error");
+    } else {
+      limit.classList.remove("message-limit-error");
+    }
   }
 
   return (
@@ -63,13 +79,28 @@ const Contact = () => {
           <form className="contact-input" ref={form} onSubmit={handleSubmit}>
             <label>Name</label>
             <label className="name-limit"></label>
-            <input type="text" name="user_name" maxLength={55} onChange={handleNameCheck}/>
+            <input
+              type="text"
+              name="user_name"
+              maxLength={55}
+              onChange={handleNameCheck}
+            />
             <label>Email</label>
             <label className="email-limit"></label>
-            <input type="email" name="user_email" maxLength={55} onChange={handleEmailCheck}/>
+            <input
+              type="email"
+              name="user_email"
+              maxLength={55}
+              onChange={handleEmailCheck}
+            />
             <label>Message</label>
             <label className="message-limit"></label>
-            <textarea id="message" name="message" maxLength={255} onChange={handleMessageCheck}/>
+            <textarea
+              id="message"
+              name="message"
+              maxLength={255}
+              onChange={handleMessageCheck}
+            />
             <input id="submit" type="submit" value="Send" />
           </form>
         </div>
