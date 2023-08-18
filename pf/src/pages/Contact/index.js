922ec3920e943d5c76be45dea46b2a9dc3d53d06
trisrosco <trisrosco@gmail.com>
2023-08-18 10:51:46 +0100

    animation on text area less janky. Won't maintain size though

--- pf/src/pages/Contact/index.js
+++ pf/src/pages/Contact/index.js
@@ -10,6 +10,28 @@ const Contact = () => {
 
   const form = useRef();
 
+  const checkValidity = (event) => {
+    let name = document.getElementsByName("user_name")[0];
+    let email = document.getElementsByName("user_email")[0];
+    let message = document.getElementById("message");
+
+    if (name.value.length < 1) {
+      alert("Name is empty");
+      event.preventDefault();
+    } else if (email.value.length < 1) {
+      alert("Email is empty");
+      event.preventDefault();
+    } else if (message.value.length < 1) {
+      alert("Please enter a message");
+      event.preventDefault();
+    } else if (message.value.length < 10) {
+      alert("Message is too short");
+      event.preventDefault();
+    } else {
+      handleSubmit(event);
+    }
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("Button pressed");
@@ -37,7 +59,7 @@ const Contact = () => {
     let limit = document.getElementsByClassName("name-limit")[0];
     limit.innerHTML = input.value.length + "/55";
 
-    if (input.value.length > 54) {
+    if (input.value.length > 54 || input.value.length < 2) {
       limit.classList.add("name-limit-error");
     } else {
       limit.classList.remove("name-limit-error");
@@ -49,7 +71,7 @@ const Contact = () => {
     let limit = document.getElementsByClassName("email-limit")[0];
     limit.innerHTML = input.value.length + "/55";
 
-    if (input.value.length > 54) {
+    if (input.value.length > 54 || input.value.length < 5) {
       limit.classList.add("email-limit-error");
     } else {
       limit.classList.remove("email-limit-error");
@@ -61,8 +83,13 @@ const Contact = () => {
     let limit = document.getElementsByClassName("message-limit")[0];
     limit.innerHTML = input.value.length + "/255";
 
-    if (input.value.length > 254) {
+    if (input.value.length > 254 || input.value.length < 10) {
       limit.classList.add("message-limit-error");
+      limit.innerHTML = input.value.length + "/255 (Min 10 characters)";
+    }
+    if (input.value.length === 255) {
+      limit.classList.add("message-limit-error");
+      limit.innerHTML = input.value.length + "/255 (Max 255 characters)";
     } else {
       limit.classList.remove("message-limit-error");
     }
@@ -76,36 +103,36 @@ const Contact = () => {
       <div className="contact-content">
         <div className="contact-content-item">
           <p> Contact Me </p>
-          <form className="contact-input" ref={form} onSubmit={handleSubmit}>
+          <form className="contact-input" ref={form} onSubmit={checkValidity}>
             <label>Name</label>
             <div classname="input-container">
-            <label className="name-limit"></label>
-            <input
-              type="text"
-              name="user_name"
-              maxLength={55}
-              onChange={handleNameCheck}
-            />
+              <label className="name-limit"></label>
+              <input
+                type="text"
+                name="user_name"
+                maxLength={55}
+                onChange={handleNameCheck}
+              />
             </div>
             <label>Email</label>
             <div classname="input-container">
-            <label className="email-limit"></label>
-            <input
-              type="email"
-              name="user_email"
-              maxLength={55}
-              onChange={handleEmailCheck}
-            />
+              <label className="email-limit"></label>
+              <input
+                type="email"
+                name="user_email"
+                maxLength={55}
+                onChange={handleEmailCheck}
+              />
             </div>
             <label>Message</label>
             <div classname="input-container">
-            <label className="message-limit"></label>
-            <textarea
-              id="message"
-              name="message"
-              maxLength={255}
-              onChange={handleMessageCheck}
-            />
+              <label className="message-limit"></label>
+              <textarea
+                id="message"
+                name="message"
+                maxLength={255}
+                onChange={handleMessageCheck}
+              />
             </div>
             <input id="submit" type="submit" value="Send" />
           </form>
