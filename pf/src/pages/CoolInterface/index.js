95f26f6d90ec88a6a155dd6cb32c8e09dd377b40
trisrosco <trisrosco@gmail.com>
2023-08-21 11:42:43 +0100

    Basic skeleton code and routing sorted

---
+++ pf/src/pages/CoolInterface/index.js
@@ -0,0 +1,15 @@
+import { React } from "react";
+import "./styles.css";
+import NavBar from "../../components/navBar";
+
+const CoolInterface = () => {
+  <div className="interface">
+    <header className="interface-header">
+        <NavBar />
+    </header>
+    
+    
+   </div>;
+};
+
+export default CoolInterface;
