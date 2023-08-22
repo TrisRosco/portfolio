636bd87c4b1f0024759d827bb7afd6c9f342be33
trisrosco <trisrosco@gmail.com>
2023-08-22 11:17:59 +0100

    flicky switch seems to be flickable

--- pf/src/components/interface/ClickySwitch2/index.js
+++ pf/src/components/interface/ClickySwitch2/index.js
@@ -1,42 +1,43 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 import { motion, useDragControls } from "framer-motion";
-import buttonDown from "../../../assets/sounds/buttonDown.mp3";
-import buttonUp from "../../../assets/sounds/buttonUp.mp3";
+// import buttonDown from "../../../assets/sounds/buttonDown.mp3";
+// import buttonUp from "../../../assets/sounds/buttonUp.mp3";
 
 function ClickySwitch2() {
   const [isOn, setIsOn] = useState(false);
   const controls = useDragControls();
-  const audioButtonDown = new Audio(buttonDown);
-  const audioButtonUp = new Audio(buttonUp);
+  // const audioButtonDown = new Audio(buttonDown);
+  // const audioButtonUp = new Audio(buttonUp);
 
-  const playButtonDown = () => {
-    audioButtonDown.play();
-  };
+  // const playButtonDown = () => {
+  //   audioButtonDown.play();
+  // };
 
-  const playButtonUp = () => {
-    audioButtonUp.play();
-  };
+  // const playButtonUp = () => {
+  //   audioButtonUp.play();
+  // };
 
   useEffect(() => {
     vibrateDevice();
     console.log("vibrate");
   }, [isOn]);
 
+
+
   function vibrateDevice() {
     // Check if the Vibration API is supported by the browser
     if ("vibrate" in navigator) {
       navigator.vibrate(50); // Adjust the duration as needed
+
     }
   }
 
   function toggleSwitch(event, info) {
     if (info.point.x > 0) {
       setIsOn(true);
-      playButtonUp();
     } else {
       setIsOn(false);
-      playButtonUp();
     }
   }
 
@@ -57,10 +58,10 @@ function ClickySwitch2() {
           whileTap={{ scale: 0.98 }}
           drag="x"
           dragControls={controls}
-          dragConstraints={{ left: 0, right: 0 }}
+          dragConstraints={{ left: 1, right: 1 }}
           dragElastic={0.002}
-          onDragEnd={toggleSwitch}
-          onTapStart={playButtonDown}
+          dragDirectionLock={true}
+          onDrag={toggleSwitch}
         />
       </div>
     </div>
