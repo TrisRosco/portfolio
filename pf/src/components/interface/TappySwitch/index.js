fae673be782df5bd693a9a0bbee192b5d8e7a06b
trisrosco <trisrosco@gmail.com>
2023-09-04 12:09:26 +0100

    Bit closer, still sucks

--- pf/src/components/interface/TappySwitch/index.js
+++ pf/src/components/interface/TappySwitch/index.js
@@ -5,9 +5,19 @@ import "./styles.css";
 const TappySwitch = () => {
     const [isOn, setIsOn] = useState(false);
 
+    //if (isOn) is true set div to on else set div to off
+
+    //ternary operator example 
+
+    // const isOn = true;
+    // const isOn = false;
+
+    // const isOn = true ? "on" : "off";
+    
+
   return <div className="tappy-switch-outer">
     <div className="tappy-switch-inner" data-isOn={isOn}>
-        <div className="tappy-switch-handle" data-isOn={isOn} onClick={() => setIsOn(!isOn)}></div>
+        <div className="tappy-switch-handle" data-isOn={isOn} onClick={() => setIsOn(!isOn)}> { isOn ? "ON" : "OFF" } </div>
     </div>
 
   </div>;
