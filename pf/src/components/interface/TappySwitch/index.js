61234dc605c9aaeec9494196a413cf962c8526b1
trisrosco <trisrosco@gmail.com>
2023-09-18 13:10:50 +0100

    added vibrate functionality, a toggle function and sfx to TappySwitch

--- pf/src/components/interface/TappySwitch/index.js
+++ pf/src/components/interface/TappySwitch/index.js
@@ -1,8 +1,41 @@
 import React, { useState } from "react";
 import "./styles.css";
+import tapOn from "../../../assets/sounds/tapOn.mp3";
+import tapOff from "../../../assets/sounds/tapOff.mp3";
+
 
 const TappySwitch = () => {
   const [isOn, setIsOn] = useState(false);
+
+  const audioTapOn = new Audio(tapOn);
+  const audioTapOff = new Audio(tapOff);
+
+  const playTapOn = () => {
+    audioTapOn.play();
+  };
+
+  const playTapOff = () => {
+    audioTapOff.play();
+  };
+
+  function vibrateDevice() {
+    if ("vibrate" in navigator) {
+      navigator.vibrate(70); 
+    }
+  }
+
+  function toggleSwitch() {
+    if (isOn) {
+      setIsOn(false);
+      playTapOn();
+      vibrateDevice();
+    } else {
+      setIsOn(true);
+      playTapOff();
+      vibrateDevice();
+    }
+  }
+
 
   return (
     <div className="tappy-switch-outer">
@@ -11,7 +44,7 @@ const TappySwitch = () => {
         <div
           className="tappy-switch-handle"
           data-isOn={isOn}
-          onClick={() => setIsOn(!isOn)}
+          onClick={toggleSwitch}
         >
           {isOn ? "ON" : "OFF"}
         </div>
