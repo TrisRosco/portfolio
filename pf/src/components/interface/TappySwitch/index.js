e1d5e04754694ca042a75812b56fcf2fecac1dcd
trisrosco <trisrosco@gmail.com>
2023-09-18 12:19:33 +0100

    AH! I've DONE IT!

--- pf/src/components/interface/TappySwitch/index.js
+++ pf/src/components/interface/TappySwitch/index.js
@@ -1,27 +1,24 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-
 const TappySwitch = () => {
-    const [isOn, setIsOn] = useState(false);
+  const [isOn, setIsOn] = useState(false);
 
-    //if (isOn) is true set div to on else set div to off
-
-    //ternary operator example 
-
-    // const isOn = true;
-    // const isOn = false;
-
-    // const isOn = true ? "on" : "off";
-    
-
-  return <div className="tappy-switch-outer">
-    <div className="tappy-switch-inner" data-isOn={isOn}>
-        <div className="tappy-switch-handle" data-isOn={isOn} onClick={() => setIsOn(!isOn)}> { isOn ? "ON" : "OFF" } </div>
-        <span className="text">OFF ON</span>
+  return (
+    <div className="tappy-switch-outer">
+      <div className="tappy-switch-inner" data-isOn={isOn}>
+        <span className="unselected" data-isOn={isOn}>{isOn ? "OFF" : ""} </span>
+        <div
+          className="tappy-switch-handle"
+          data-isOn={isOn}
+          onClick={() => setIsOn(!isOn)}
+        >
+          {isOn ? "ON" : "OFF"}
+        </div>
+        <span className="unselected" data-isOn={isOn}>{isOn ? "" : "ON"}</span>
+      </div>
     </div>
-
-  </div>;
+  );
 };
 
 export default TappySwitch;
