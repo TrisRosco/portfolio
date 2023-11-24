e6bcc1c88392625b122287dd5dbc0cc44d4119a0
trisrosco <trisrosco@gmail.com>
2023-11-24 15:41:05 +0000

    added plan to page to show off what i'm doing

--- pf/src/pages/Webstore/index.js
+++ pf/src/pages/Webstore/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./styles.css";
 import NavBar from "../../components/navBar";
+import plan from "./plan.JPG"
 
 const Webstore = () => {
     useEffect(() => {
@@ -12,6 +13,8 @@ const Webstore = () => {
         <NavBar />
         <div className="webstore__container">
             <h1>Webstore</h1>
+            <p>Coming soon!</p>
+            <img src={plan} alt="plan" id="plan" />
         </div>
         </React.Fragment>
     );
