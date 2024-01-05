edb04372f229045f75c06619ea387846c081a952
trisrosco <trisrosco@gmail.com>
2024-01-05 12:28:55 +0000

    Set up env

--- pf/src/backend/server.js
+++ pf/src/backend/server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = 3001;
 
+const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
